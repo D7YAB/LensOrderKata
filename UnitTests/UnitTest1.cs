c9a5831a8790d9aced733797050a66c882cf7b67
Diyab.Hossain@outlook.com <Diyab.Hossain@outlook.com>
2025-11-27 21:33:10 +0000

    Add project files.

---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestPlatform.Utilities;
+using System.Collections.Generic;
+
+namespace UnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        // You’re building a basic lens ordering system for an optometry practice. Each lens type has a code and a price.
+
+        //Lens Codes
+        //Code Description     Price
+        //SV01 Single Vision   £50
+        //BF02 Bifocal         £75
+        //VF03 Varifocal       £100
+
+        //Requirements
+        //•Accept a list of lens codes.
+        //•Calculate the total cost.
+        //•Ignore invalid codes.
+        //•Return a summary of lens types and total cost.
+
+        //Example Input
+        //SV01, VF03, SV01, BF02
+
+        //Expected Output
+        //SV01 x2 = £100
+        //VF03 x1 = £100
+        //BF02 x1 = £75
+        //Total = £275
+    }
+}
