4ec3031841375527567526df37d7820c98fa1913
Diyab.Hossain@outlook.com <Diyab.Hossain@outlook.com>
2025-11-27 21:59:08 +0000

    Created ParseCsvToCodes to take a string input and return list of individual code values

---
+++ LensOrderKata/LensCodeParser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LensOrderKata
+{
+    public class LensCodeParser
+    {
+        /// <summary>
+        /// Parses a comma-separated string and returns a list of individual code values.
+        /// </summary>
+        /// <param name="csv">A string containing code values separated by commas.</param>
+        /// <returns>A list of strings representing the individual codes parsed from the input.</returns>
+        public List<string> ParseCsvToCodes(string csv)
+        {
+            var parsedCodes = csv.Split(',').ToList();
+
+            return parsedCodes;
+        }
+    }
+}
