f6d6e4b0bcdd609fd16bea2b2442f2d2522eb940
Diyab.Hossain@outlook.com <Diyab.Hossain@outlook.com>
2025-11-27 22:26:08 +0000

    Created Lens class

---
+++ LensOrderKata/Lens.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LensOrderKata
+{
+    /// <summary>
+    /// Represents a lens with its code, description, and price.
+    /// </summary>
+    public class Lens
+    {
+        string Code { get; set; } = string.Empty;
+        string Description { get; set; } = string.Empty;
+        double Price { get; set; } = 0.0;
+    }
+}
